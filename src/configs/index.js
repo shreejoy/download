12c62b0dbc2cb8ef25a382f9303b47102808fb5a
ArunTeltia <teltia.arun18@gmail.com>
2021-01-22 21:28:20 +0530

    deployment

--- src/configs/index.js
+++
@@ -1,6 +0,0 @@
-export default {
-  APP_NAME: 'Superior OS | Downloads',
-  APP_SUBNAME: 'Superior OS ',
-  APP_DESC: 'Superior OS download page for official devices',
-  APP_LOGO: '/img/logo.png',
-};
